8693d3f6e3b020ce2c21bb00f2f63d8ccbe2a9d9
bobruk76 <vladimir.m.polyakov@gmail.com>
2020-11-10 18:40:42 +0300

    add admin user and general models

--- hrapp/admin.py
+++ hrapp/admin.py
@@ -1,3 +1,16 @@
-from django.contrib import admin
 
+from django.contrib import admin
+from hrapp.models import Question, Answer
 # Register your models here.
+
+
+
+@admin.register(Question)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+@admin.register(Answer)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
