edd063092126f71b559a2e834cf8909a97a756a1
bobruk76 <vladimir.m.polyakov@gmail.com>
2020-12-01 16:51:51 +0300

    fix serializer

--- hrapp/serializers.py
+++ hrapp/serializers.py
@@ -11,11 +11,30 @@ class AnswerSerializer(serializers.ModelSerializer):
 
 class QuestionSerializer(serializers.ModelSerializer):
     # answers = AnswerSerializer(many=True)
-    # answers = serializers.SerializerMethodField()
+    answers = serializers.SerializerMethodField()
 
     class Meta:
         model = Question
         fields = ['id', 'content', 'answers', ]
+    
+    def get_answers(self, obj):
+        return_data = None
+        if type(obj.answers) == list:
+            embedded_list = []
+            for item in obj.answers:
+                embedded_dict = item.__dict__
+                for key in list(embedded_dict.keys()):
+                    if key.startswith('_'):
+                        embedded_dict.pop(key)
+                embedded_list.append(embedded_dict)
+            return_data = embedded_list
+        else:
+            embedded_dict = obj.answers.__dict__
+            for key in list(embedded_dict.keys()):
+                if key.startswith('_'):
+                    embedded_dict.pop(key)
+            return_data = embedded_dict
+        return return_data
 
 
 # {
